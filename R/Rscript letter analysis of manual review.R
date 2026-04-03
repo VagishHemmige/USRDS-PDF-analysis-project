5ceac3b6ca3f9a2febf8760d5790609b634417cd
Vagish Hemmige <vahemmig@montefiore.org>
2026-04-03 17:18:57 -0400

    Letter table creation

--- R/Rscript letter analysis of manual review.R
+++ R/Rscript letter analysis of manual review.R
@@ -127,6 +127,48 @@ Anagha_summer_project_form_final %>%
   as_gt() %>%
   gtsave(filename = "Letter analysis/Letter analysis Table 1 overall raw.html")
 
+#Create variable label list
+label_list = list(
+  is_this_paper_a_new_data_analysis ~ "New data analysis",
+  which_type_of_transplant_study_is_this ~ "Type of transplant study",
+  did_the_authors_state_what_programming_language_they_used ~ "Programming language stated",
+  did_the_authors_explicitly_state_what_files_they_used ~ "USRDS files explicitly stated",
+  which_pieces_of_data_were_abstracted_from_the_files ~ "Data elements abstracted",
+  does_the_choice_of_files_codes_seem_appropriate_for_the_research_question ~ "Files/codes appropriate for research question",
+  comments_16 ~ "Comments on file/code choice",
+  did_the_authors_share_the_code_they_used ~ "Authors shared code",
+  was_the_study_funded ~ "Study funded",
+  who_funded_the_study ~ "Study funder",
+  would_the_major_data_tasks_needed_for_this_study_be_covered_by_currently_proposed_usrds_r_package ~ "Covered by proposed usRds package",
+  which_tasks_would_not_be_covered ~ "Tasks not covered by usRds",
+  were_any_crosswalks_used ~ "Any crosswalks used",
+  which_crosswalks_were_used ~ "Crosswalks used",
+  confirmed_by_vh ~ "Confirmed by VH",
+  changes_made ~ "Changes made after review",
+  comments_26 ~ "Reviewer comments",
+  language_r ~ "R used",
+  language_sas ~ "SAS used",
+  language_stata ~ "Stata used",
+  language_spss ~ "SPSS used",
+  file_core ~ "Core file used",
+  file_institution ~ "Institution file used",
+  file_ps ~ "Physician/Supplier file used",
+  file_hospital ~ "Hospital file used",
+  file_transplant ~ "Transplant file used",
+  file_partd ~ "Part D file used",
+  file_CROWNWeb ~ "CROWNWeb file used",
+  component_basicdemographics ~ "Basic demographics abstracted",
+  component_icd9 ~ "ICD-9 codes abstracted",
+  component_icd10 ~ "ICD-10 codes abstracted",
+  component_cptcode ~ "CPT codes abstracted",
+  component_medicationD ~ "Part D medication data abstracted",
+  component_transplant ~ "Transplant-specific data abstracted",
+  component_costs ~ "Cost data abstracted",
+  component_residential ~ "Residential location abstracted"
+)
+
+
+
 
 Anagha_summer_project_form_final %>%
   select(
@@ -138,7 +180,49 @@ Anagha_summer_project_form_final %>%
     -year,
     -what_programming_language_s_was_were_used,-what_files_were_used
   ) %>%
-  tbl_summary(by = is_this_a_transplant_related_study) %>%
+  tbl_summary(by = is_this_a_transplant_related_study,
+              label=label_list
+              ) %>%
+  
+  add_variable_group_header(
+    header = "Data components abstracted",
+    variables = c(
+      component_basicdemographics,
+      component_icd9,
+      component_icd10,
+      component_cptcode,
+      component_medicationD,
+      component_transplant,
+      component_costs,
+      component_residential
+    )
+  )%>%
+  
+  add_variable_group_header(
+    header = "Files used",
+    variables = c(
+      file_core,
+      file_institution,
+      file_ps,
+      file_hospital,
+      file_transplant,
+      file_partd,
+      file_CROWNWeb
+    
+    )
+  )%>%
+  
+  add_variable_group_header(
+    header = "Files used",
+    variables = c(
+      language_r,
+      language_sas,
+      language_stata,
+      language_spss
+      
+    )
+  )%>%
+  
   as_gt() %>%
   gtsave(filename = "Letter analysis/Letter analysis Table 2 stratified by transplant.html")
 
@@ -150,7 +234,8 @@ Anagha_summer_project_form_split[["transplant"]] %>%
     -title,
     -journal,
     -year,
-    -what_programming_language_s_was_were_used,-what_files_were_used
+    -what_programming_language_s_was_were_used,
+    -what_files_were_used
   ) %>%
   tbl_summary(by = which_type_of_transplant_study_is_this) %>%
   as_gt() %>%
