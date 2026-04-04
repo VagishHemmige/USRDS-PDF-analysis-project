a1ce676ff29e5f36aa1c47089713cc6d49a5ccd4
Vagish Hemmige <vahemmig@montefiore.org>
2026-04-03 20:34:21 -0400

    Update table creation script for letter

--- R/Rscript letter analysis of manual review.R
+++ R/Rscript letter analysis of manual review.R
@@ -7,6 +7,7 @@ library(janitor)
 library(readxl)
 library(gtsummary)
 library(gt)
+library(flowchart)
 
 
 # ---- Import data ----
@@ -16,20 +17,24 @@ Anagha_summer_project_form_2025_raw <- read_excel("data-raw/Data abstraction Exc
 # ---- Clean data ----
 
 Anagha_summer_project_form_clean <- Anagha_summer_project_form_2025_raw %>%
-  clean_names() %>%
+  clean_names()%>%
   filter(confirmed_by_vh == "Yes")
 
-Anagha_summer_project_form_clean %>%
-  count(is_this_paper_a_new_data_analysis == "Yes")
-Anagha_summer_project_form_clean <- Anagha_summer_project_form_clean %>%
-  filter(is_this_paper_a_new_data_analysis == "Yes")
+#Create flowchart of STROBE diagram
+Anagha_summer_project_form_clean_fc<-Anagha_summer_project_form_clean%>%
+  as_fc(label = "Initial papers from Pubmed search")%>%
+  fc_filter(is_this_paper_a_new_data_analysis == "Yes",
+            label="New patient-level data analysis",
+            show_exc = TRUE)%>%
+  
+  fc_split(is_this_a_transplant_related_study,
+           label=c("Not transplant-related", "Transplant-related"))
 
-Anagha_summer_project_form_clean %>%
-  count(is_this_a_transplant_related_study == "No")
-Anagha_summer_project_form_clean %>%
-  count(is_this_a_transplant_related_study == "Yes")
+fc_draw(Anagha_summer_project_form_clean_fc)%>%
+  fc_export("Letter analysis/flowchart.png")
 
-Anagha_summer_project_form_final <- Anagha_summer_project_form_clean %>%
+#Create final dataset for analysis
+Anagha_summer_project_form_final <- Anagha_summer_project_form_clean_fc$data %>%
   
   #Language variables
   mutate(language_r = str_detect(what_programming_language_s_was_were_used, "R")) %>%
@@ -96,13 +101,13 @@ Anagha_summer_project_form_final <- Anagha_summer_project_form_clean %>%
 
 #Split the final data set into non-transplant and transplant components
 Anagha_summer_project_form_split <- Anagha_summer_project_form_final %>%
-group_by(is_this_a_transplant_related_study) %>%
+  group_by(is_this_a_transplant_related_study) %>%
   group_split() %>%
   set_names(c("not_transplant", "transplant"))
 
 
-# ---- Analysis and table creation ----
 
+# ---- Analysis and table creation ----
 
 #Save the overall table as a document
 Anagha_summer_project_form_final %>%
@@ -133,8 +138,6 @@ Anagha_summer_project_form_final %>%
 
 #Create variable label list
 label_list = list(
-  is_this_paper_a_new_data_analysis ~ "New data analysis",
-  which_type_of_transplant_study_is_this ~ "Type of transplant study",
   did_the_authors_state_what_programming_language_they_used ~ "Programming language stated",
   did_the_authors_explicitly_state_what_files_they_used ~ "USRDS files explicitly stated",
   does_the_choice_of_files_codes_seem_appropriate_for_the_research_question ~ "Files/codes appropriate for research question",
@@ -142,8 +145,6 @@ label_list = list(
   was_the_study_funded ~ "Study funded",
   would_the_major_data_tasks_needed_for_this_study_be_covered_by_currently_proposed_usrds_r_package ~ "Covered by proposed usRds package",
   were_any_crosswalks_used ~ "Any crosswalks used",
-  which_crosswalks_were_used ~ "Crosswalks used",
-  changes_made ~ "Changes made after review",
   language_r ~ "R used",
   language_sas ~ "SAS used",
   language_stata ~ "Stata used",
@@ -169,26 +170,51 @@ label_list = list(
 
 
 Anagha_summer_project_form_final %>%
+  
   select(
-    -filepath,
-    -timestamp,
-    -authors,
-    -title,
-    -journal,
-    -year,
-    -what_programming_language_s_was_were_used,-what_files_were_used,
-    -which_pieces_of_data_were_abstracted_from_the_files,
-    -comments_16,
-    -comments_26,
-    -confirmed_by_vh,
-    -who_funded_the_study,
-    -which_tasks_would_not_be_covered
+    is_this_a_transplant_related_study,
+    
+    did_the_authors_state_what_programming_language_they_used,
+    language_r,
+    language_sas,
+    language_stata,
+    language_spss,
+    
+    did_the_authors_explicitly_state_what_files_they_used,
+    
+    does_the_choice_of_files_codes_seem_appropriate_for_the_research_question,
+    file_core,
+    file_institution,
+    file_ps,
+    file_hospital,
+    file_transplant,
+    file_partd,
+    file_CROWNWeb,
+
+    component_basicdemographics,
+    component_icd9,
+    component_icd10,
+    component_cptcode,
+    component_medicationD,
+    component_transplant,
+    component_costs,
+    component_residential,
+
+    were_any_crosswalks_used,
+            
+    did_the_authors_share_the_code_they_used,
+    was_the_study_funded,
+    would_the_major_data_tasks_needed_for_this_study_be_covered_by_currently_proposed_usrds_r_package
+
+
   ) %>%
+  
   tbl_summary(by = is_this_a_transplant_related_study,
               label=label_list,
               missing = "no"
-              ) %>%
-  
+  ) %>%
+
+  modify_spanning_header(all_stat_cols() ~ "**Transplant-related study?**")%>%
   add_variable_group_header(
     header = "Data components abstracted",
     variables = c(
@@ -213,12 +239,12 @@ Anagha_summer_project_form_final %>%
       file_transplant,
       file_partd,
       file_CROWNWeb
-    
+      
     )
   )%>%
   
   add_variable_group_header(
-    header = "Files used",
+    header = "Programming language used",
     variables = c(
       language_r,
       language_sas,
@@ -232,16 +258,90 @@ Anagha_summer_project_form_final %>%
   gtsave(filename = "Letter analysis/Letter analysis Table 2 stratified by transplant.html")
 
 Anagha_summer_project_form_split[["transplant"]] %>%
+  
+  
   select(
-    -filepath,
-    -timestamp,
-    -authors,
-    -title,
-    -journal,
-    -year,
-    -what_programming_language_s_was_were_used,
-    -what_files_were_used
-  ) %>%
-  tbl_summary(by = which_type_of_transplant_study_is_this) %>%
+    which_type_of_transplant_study_is_this,
+    
+    did_the_authors_state_what_programming_language_they_used,
+    language_r,
+    language_sas,
+    language_stata,
+    language_spss,
+    
+    did_the_authors_explicitly_state_what_files_they_used,
+    
+    does_the_choice_of_files_codes_seem_appropriate_for_the_research_question,
+    file_core,
+    file_institution,
+    file_ps,
+    file_hospital,
+    file_transplant,
+    file_partd,
+    file_CROWNWeb,
+    
+    component_basicdemographics,
+    component_icd9,
+    component_icd10,
+    component_cptcode,
+    component_medicationD,
+    component_transplant,
+    component_costs,
+    component_residential,
+    
+    were_any_crosswalks_used,
+    
+    did_the_authors_share_the_code_they_used,
+    was_the_study_funded,
+    would_the_major_data_tasks_needed_for_this_study_be_covered_by_currently_proposed_usrds_r_package
+    
+    
+  )%>%
+  
+
+  tbl_summary(by = which_type_of_transplant_study_is_this,
+              label=label_list,
+              missing = "no") %>%
+  
+    modify_spanning_header(all_stat_cols() ~ "**Type of transplant study**")%>%
+    add_variable_group_header(
+      header = "Data components abstracted",
+      variables = c(
+        component_basicdemographics,
+        component_icd9,
+        component_icd10,
+        component_cptcode,
+        component_medicationD,
+        component_transplant,
+        component_costs,
+        component_residential
+      )
+    )%>%
+    
+    add_variable_group_header(
+      header = "Files used",
+      variables = c(
+        file_core,
+        file_institution,
+        file_ps,
+        file_hospital,
+        file_transplant,
+        file_partd,
+        file_CROWNWeb
+        
+      )
+    )%>%
+    
+    add_variable_group_header(
+      header = "Programming language used",
+      variables = c(
+        language_r,
+        language_sas,
+        language_stata,
+        language_spss
+        
+      )
+    )%>%
+    
   as_gt() %>%
   gtsave(filename = "Letter analysis/Letter analysis Table 3 stratified by transplant study type.html")
